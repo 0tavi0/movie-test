8489c6e40847efbef8371df88ae85382ae612ef0
Otavio Augusto <otavio.leao@iteris.com.br>
2020-04-02 12:52:15 -0300

    teste

---
+++ app/src/main/java/com/example/movie_test/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.movie_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movie_test.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
